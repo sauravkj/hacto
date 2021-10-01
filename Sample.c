44f7624483d5ca281d98f0eef67a831d13fa6870
Sankar Raj <71374835+SankarRaj432@users.noreply.github.com>
2021-10-01 16:43:36 +0530

    Update Sample.c

--- Sample.c
+++ Sample.c
@@ -1,12 +1,19 @@
 #include<stdio.h>  
-int main()    
+int main(){    
+int n,i,m=0,flag=0;    
+printf("Enter the number to check prime:");    
+scanf("%d",&n);    
+m=n/2;    
+for(i=2;i<=m;i++)    
 {    
- int i,fact=1,number;    
- printf("Enter a number: ");    
-  scanf("%d",&number);    
-    for(i=1;i<=number;i++){    
-      fact=fact*i;    
-  }    
-  printf("Factorial of %d is: %d",number,fact);    
+if(n%i==0)    
+{    
+printf("Number is not prime");    
+flag=1;    
+break;    
+}    
+}    
+if(flag==0)    
+printf("Number is prime");     
 return 0;  
-}   
+ }    
